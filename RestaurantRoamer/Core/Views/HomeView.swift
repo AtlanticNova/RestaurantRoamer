72bb0f3dd10c650bce6aa762fef04ee3b941d85f
Martinus Andika Novanawa <69181762+AtlanticNova@users.noreply.github.com>
2023-05-24 19:23:16 +0700

    Initialize Project

---
+++ RestaurantRoamer/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  RestaurantRoamer
+//
+//  Created by Martinus Andika Novanawa on 24/05/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
